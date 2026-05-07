391ca71bec6ec02d7f422d466e6c638f3a24fa11
Malakaka <malakalsenine@gmail.com>
2026-05-07 11:47:04 +0400

    Step 1: Created base class Employee with encapsulation and virtual Work method

--- solution01/project01/Program.cs
+++ solution01/project01/Program.cs
@@ -2,9 +2,65 @@
 {
     internal class Program
     {
+        // ===============================
+        //1. Create a Base Class
+        // ===============================
+
+
+        public class Employee
+        {
+            //Private fields (Encapsulation)
+            private int id;
+            private double salary;
+
+            // Property for ID with validation
+
+            public int Id
+            {
+                get { return id; }
+                set
+                {
+                    if (value > 0)
+                    
+                        id = value;
+                    }
+                }
+
+                // Property for Salary with validation
+
+                public double Salary
+            {
+                get { return salary; }
+                set
+                {
+                    if (value >= 0 ) 
+                        salary = value;
+                }
+            }
+
+            // Public properties
+
+            public string Name { get; set; }
+            public string Department { get; set; }
+
+            // Virtual method (Polymorphism)
+
+            public virtual void work()
+            {
+                Console.WriteLine("Employee is working");
+            }
+
+        }
+
+       
+
+
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
         }
     }
 }
